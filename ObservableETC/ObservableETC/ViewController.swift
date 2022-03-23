b0adef906419b82d267dbf1c0f9d0dc2e8b291a2
Beepeach <eh7457303j@gmail.com>
2022-03-23 23:30:05 +0900

    Add: Observable ETC 포스팅 추가

---
+++ ObservableETC/ObservableETC/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ObservableETC
+//
+//  Created by JunHeeJo on 3/23/22.
+//
+
+import UIKit
+import RxSwift
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // MARK: - Factory
+        // 각각 구독자에 대해 새로운 observable을 생성하는 observable factory를 생성 가능
+        // deferred 오퍼레이터를 이용하면 됩니다.
+        let disposeBag: DisposeBag = DisposeBag()
+
+        var flip: Bool = false
+
+        let factory: Observable<Int> = Observable.deferred {
+            flip.toggle()
+            
+            if flip {
+                return Observable.of(2, 4, 6)
+            } else {
+                return Observable.of(1, 3, 5)
+            }
+        }
+
+        // 외부적으로는 observable factory는 구별이 가지 않습니다.
+        for _ in 0...3 {
+            factory.subscribe(onNext: {
+                print($0, terminator: "")
+            }).disposed(by: disposeBag)
+            
+            print()
+        }
+    }
+}
+
