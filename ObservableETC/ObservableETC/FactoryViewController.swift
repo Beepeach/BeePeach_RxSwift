9374f5e85ef94906e6c8b5895c4529df5721ba1b
Beepeach <eh7457303j@gmail.com>
2022-04-19 11:17:30 +0900

    Refact: 소주제를 VC별로 나누는 작업
    ETC라는 커다란 VC가 아닌 Trait, Factory별로 나누었습니다.

---
+++ ObservableETC/ObservableETC/FactoryViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FactoryViewController.swift
+//  ObservableETC
+//
+//  Created by JunHeeJo on 4/19/22.
+//
+
+import UIKit
+
+class FactoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // MARK: - Factory
+        // 각각 구독자에 대해 새로운 observable을 생성하는 observable factory를 생성 가능
+        // deferred 오퍼레이터를 이용하면 됩니다.
+        let disposeBag: DisposeBag = DisposeBag()
+        var isOdd: Bool = false
+
+        let factory: Observable<Int> = Observable.deferred {
+            isOdd.toggle()
+            
+            if isOdd {
+                return Observable.of(1, 3, 5)
+            } else {
+                return Observable.of(2, 4, 6)
+            }
+        }
+
+        // 외부적으로는 observable factory는 observable과 구별이 가지 않습니다.
+        for _ in 0...3 {
+            factory.subscribe(onNext: {
+                print($0, terminator: " ")
+            }).disposed(by: disposeBag)
+            
+            print()
+        }
+    }
+}
--- ObservableETC/ObservableETC/TraitViewController.swift
+++ ObservableETC/ObservableETC/TraitViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TraitViewController.swift
 //  ObservableETC
 //
 //  Created by JunHeeJo on 3/23/22.
@@ -9,37 +9,11 @@ import UIKit
 import RxSwift
 
 
-class ViewController: UIViewController {
-
+class TraitViewController: UIViewController {
+    var disposeBag: DisposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // MARK: - Factory
-        // 각각 구독자에 대해 새로운 observable을 생성하는 observable factory를 생성 가능
-        // deferred 오퍼레이터를 이용하면 됩니다.
-        let disposeBag: DisposeBag = DisposeBag()
-        var isOdd: Bool = false
-
-        let factory: Observable<Int> = Observable.deferred {
-            isOdd.toggle()
-            
-            if isOdd {
-                return Observable.of(1, 3, 5)
-            } else {
-                return Observable.of(2, 4, 6)
-            }
-        }
-
-        // 외부적으로는 observable factory는 observable과 구별이 가지 않습니다.
-        for _ in 0...3 {
-            factory.subscribe(onNext: {
-                print($0, terminator: " ")
-            }).disposed(by: disposeBag)
-            
-            print()
-        }
-        
-        
         // MARK: - Traits
         
         // MARK: Single
@@ -64,7 +38,9 @@ class ViewController: UIViewController {
         
         // 예시
         enum FileReadError: Error {
-            case fileNotFound, unreadable, encodingFailed
+            case fileNotFound
+            case unreadable
+            case encodingFailed
         }
         
         func loadText(from name: String) -> Single<String> {
